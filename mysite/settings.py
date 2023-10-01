1122fae659c85230d790eb465f125cdf4f77e146
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2023-10-01 18:55:20 -0400

    Add LOGIN_REDIRECT_URL

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 
+LOGIN_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
