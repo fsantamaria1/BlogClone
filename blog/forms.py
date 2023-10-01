fafee3f79289bdf9c245a2ba91f712059d51ee4a
Fredy Santamaria <fsanta3@wgu.edu>
2023-10-01 18:34:41 -0400

    Add widgets to each form

--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta():
         model = Post
         fields = ('author', 'title', 'text')
+        
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
 
 
 class CommentForm(forms.ModelForm):
@@ -14,3 +19,9 @@ class CommentForm(forms.ModelForm):
     class Meta():
         model = Comment
         fields = ('author', 'text')
+        
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+        }
+        
