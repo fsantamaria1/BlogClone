9e3880c50ff73b3a50dfb77a021f60209f667067
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2023-10-07 14:24:56 -0400

    Add the add_comment_to_post view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (
     TemplateView, 
     ListView, 
@@ -11,6 +11,7 @@ from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 
 
@@ -59,3 +60,25 @@ class DraftListView(LoginRequiredMixin, ListView):
         #They are considered drafts if they have no published date, so we filter for that.
         return Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     
+
+############################################################################################
+############################################################################################
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return redirect('post_detail', pk=post.pk)
+        
+    else:
+        form = CommentForm()
+        
+    return render(request, 'blog/comment_form.html', {'form':form})
