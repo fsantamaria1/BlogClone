c063c2497e2cb9ed0cec740adc9407e775283500
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2023-10-07 13:58:46 -0400

    Add the AboutView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import (
+    TemplateView, 
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView,
+    )
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
